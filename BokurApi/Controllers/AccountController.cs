3bb36e6039547d83e5251acda1beed16981c3974
Adam Tovatt <sporedenstore@hotmail.com>
2024-01-04 21:36:55 +0100

    add endpoint for getting all accounts

---
+++ BokurApi/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using BokurApi.Models.Http;
+using BokurApi.RateLimiting;
+using BokurApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BokurApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("get-all")]
+        [Limit(MaxRequests = 20, TimeWindow = 10)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Created)]
+        public async Task<ObjectResult> GetAllAccounts()
+        {
+            return new ApiResponse(await AccountRepository.Instance.GetAllAsync());
+        }
+    }
+}
